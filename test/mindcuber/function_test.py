87946c1d48d6029c7001e34aa2798d478061e270
nathandangg <thanhnhantdn@gmail.com>
2023-02-03 14:57:54 +0800

    added mindcuber to test

---
+++ test/mindcuber/function_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from mindcuber import MindCuber
+import mqtt_comm as com
+import logging
+import json
+import sys
+
+
+
+# logging.basicConfig(filename='rubiks.log',
+logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(filename)12s %(levelname)8s: %(message)s')
+log = logging.getLogger(__name__)
+
+# Color the errors and warnings in red
+logging.addLevelName(logging.ERROR, "\033[91m   %s\033[0m" % logging.getLevelName(logging.ERROR))
+logging.addLevelName(logging.WARNING, "\033[91m %s\033[0m" % logging.getLevelName(logging.WARNING))
+
+mcube = MindCuber()
+
+try:
+    mcube.wait_for_cube_insert()
+
+    # Push the cube to the right so that it is in the expected
+    # position when we begin scanning
+    mcube.flipper_hold_cube(100)
+    mcube.flipper_away(100)
+
+    mcube.scan()
+    mcube.rotate_cube(-1, 1)
+    mcube.flip()
+    mcube.flipper_away()
+    mcube.rotate_cube(1, 1)
+
+    scan_result_as_string = str(json.dumps(mcube.colors))
+    com.send_to_pc(scan_result_as_string)
+
+    mcube.shutdown_robot()
+
+except Exception as e:
+    log.exception(e)
+    mcube.shutdown_robot()
+    sys.exit(1)
