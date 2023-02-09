f78ce3a1d68a2445a513aa3df690d8b143e3362e
nathandangg <thanhnhantdn@gmail.com>
2023-02-09 09:48:05 +0800

    fixed bug in korf's

--- pc_side/solver.py
+++ pc_side/solver.py
@@ -10,7 +10,7 @@ def solve(max_length, time_out, cubestring, method):
         solution = twophase.solve(cubestring,max_length,time_out)
         solution = solution[:solution.strip().rfind(" ")]
     # elif method == 2:
-    #     solution = korf.solve()
+    #     solution = korf.solve(cubestring)
     #     solution = solution[:solution.strip().rfind(" ")]
     print(solution)
     return solution
