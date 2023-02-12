32f8d684ebb895944c5f8453ac988d7915b26a88
tin1307 <thanhtin001@ntu.edu.sg>
2023-02-12 21:42:19 +0800

    Add loop attempting connect to ssh client

--- pc_side/test.py
+++ pc_side/test.py
@@ -7,11 +7,12 @@ import threading
 import logging
 import time
 import pexpect
-
+logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(filename)12s %(levelname)8s: %(message)s')
+logging = logging.getLogger(__name__)
 class EV3():
     # Initialize User Config for SSH connection
     def __init__(self,**kw) :
-        self.event = threading.Event()
         self.user = kw.get('username','robot')
         self.password = kw.get('password','maker')
         self.ip = kw.get('ip','')
@@ -19,9 +20,7 @@ class EV3():
         logging.info("IP of {} loaded from config: {}".format(self.user, self.ip))
     
         # Threading looping connection try
-        self.lock = threading.Lock()
         self.running = False
-        self.counter= threading.Thread(target = self.time_loop, daemon = True)
 
         # # Spawning connection
         # self.spawn_ssh()
@@ -36,13 +35,12 @@ class EV3():
             logging.info("Logged in EV3")
             self.status = True
             self.running = False
-            self.counter.start()
             return
         except:
-            logging.debug("Failed to connect to EV3")
+            logging.info("Failed to connect to EV3")
             self.status = False
             if not self.status and not self.running:
-                threading.Thread(target = self.run, daemon = True).start()
+                threading.Thread(target = self.run).start()
                 self.running = True
             return
 
@@ -55,29 +53,16 @@ class EV3():
     def run(self):
         while not self.status:
             time.sleep(5)
-            logging.debug("Attempting to start again")
+            logging.info("Attempting to start again")
             self.spawn_ssh()
         return
 
-    def time_loop(self, num=0):
-        # This function helps to keep the ssh always online inside the ssh client
-        while True:
-            if self.event.is_set():
-                self.event.clear()
-                num = 0
-            if num>=10:
-                if not self.status:
-                    self.spawn_ssh()
-                self.ask_status()
-                num= 0
-            time.sleep(1)
-            num+=1
-
-ev3_ip = ""
+ev3_ip = "127.0.0.1"
 order = ["funtion_test",]
 ev3 = EV3(ip = ev3_ip)
 try:        
     ev3.spawn_ssh()
+    pass
     # for i in order:
     #     ev3.run_file_name(i+'.py')
 except Exception as e:
